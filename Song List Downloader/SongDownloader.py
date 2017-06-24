ffdfe5295e5628209b8950c55ea72e9f02daadad
Sushaanth P <psushaanth23@gmail.com>
2017-06-24 16:05:23 +0530

    Download list of songs

--- Song List Downloader/SongDownloader.py
+++ Song List Downloader/SongDownloader.py
@@ -1,1 +1,56 @@
+'''
+Download any song
+Author : Sushaanth P
 
+Sample Input - 
+
+5
+Kygo It Ain't Me 
+Gryffin & Illenium ft. Daya - Feel Good
+Taylor Swift- We Are Never Ever Getting Back Together
+Alesso Years
+Adele- Rolling in the Deep
+
+'''
+
+import urllib
+import urllib.request as urllib2
+from bs4 import BeautifulSoup
+
+
+def save(link, SaveAs):
+	urllib2.urlretrieve(link, SaveAs)
+
+
+def getLink(url, search, attr):
+	query = urllib.parse.quote(search)
+	response = urllib2.urlopen(url + query)
+	soup = BeautifulSoup(response.read(),"lxml")
+	for vid in soup.findAll(attrs={'class':attr})[0:1]:
+		return vid['href'] 
+
+def download(SongName):
+	SongToSearch = SongName+' lyrics'
+
+	#get the video link from youtube
+	getURL = "https://www.youtube.com/results?search_query="
+	attr = 'yt-uix-tile-link'
+	div = getLink(getURL, SongToSearch, attr)
+	songLink = 'https://www.youtube.com' + div
+
+	#get the download link using the video link
+	getURL = "https://www.youtubeinmp3.com/download/?video="
+	attr = 'button'
+	div = getLink(getURL, songLink, attr)
+	downloadLink = 'https://www.youtubeinmp3.com'+div
+
+	save(downloadLink, SongName+'.mp3')
+
+n = int(input())
+
+for i in range(n):
+	song = input()
+	print('>>> Downloading Song : \t'+song)
+	download(song)
+	print('>>> '+(str(i+1))+' down '+str(n-i-1)+' to go\t\t'+song+' is Done\n')
+			
